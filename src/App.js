ca88c6048b81fb8a5143f69e9ee84ff785ff3f4c
Kiran Borge <borgekiran123@gmail.com>
2023-10-17 12:45:47 -0700

    Added all Files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// App.js
+
+import React from 'react';
+import './App.css'; // You can create this file for styling
+
+function App() {
+  return (
+   <>
+    header
+   </>
+  );
+}
+
+export default App;
